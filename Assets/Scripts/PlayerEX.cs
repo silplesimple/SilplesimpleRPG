bccacce97553c6ad3c5d0aa1366ac15eff30f25c
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-02-22 10:06:55 +0900

    유니티2D로그라이크 에셋 추가

--- Assets/Scripts/PlayerEX.cs
+++ Assets/Scripts/PlayerEX.cs
@@ -1,18 +1,53 @@
+using EnumTypes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerEX : MonoBehaviour
+namespace EnumTypes
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public enum AttackTypes
+    { 
+        None,Melee, Range
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum CardRanks
+    { 
+        Normal,Special,Rare
+    }
+    
+    public enum CardHowToUses
     {
+        Normal,TargetGround,TargetEntity
+    }
+
+    public enum CardAfterUses
+    {
+        Discard,Destruct,Spawn
+    }
+
+    public enum GameFlowState
+    {
+        InitGame,SelectStage,Setting,Wave,EventFlow,Ending
+    }
+}
+
+namespace Structs
+{
+    [SerializeField]
+    public struct AttackData
+    {
+        public AttackTypes attackType;
+        public int attackAnimationIndex;
+
         
     }
+    [SerializeField]
+    public struct StatModifierData
+    {
+        public StatType
+    }
+}
+public class PlayerEX : MonoBehaviour
+{
+    AttackTypes attackTypes = 
 }
